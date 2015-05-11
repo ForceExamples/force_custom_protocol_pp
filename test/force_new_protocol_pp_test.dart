ba6b7ab088bc7b6acc86fa2e798a1de3081e9339
joris hermans <JHermans85@hotmail.com>
2015-05-11 12:58:26 +0200

    add tests

--- test/force_new_protocol_pp_test.dart
+++ test/force_new_protocol_pp_test.dart
@@ -1,16 +1,29 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2015, Joris Hermans. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 library force_new_protocol_pp_test;
 
 import 'package:force_custom_protocol_pp/pingpong.dart';
 import 'package:unittest/unittest.dart';
+import 'package:force/force_serverside.dart';
+import 'dart:io';
+import 'dart:convert';
 
 void main() => defineTests();
 
 void defineTests() {
+  HttpRequest req;
+    
   group('main tests', () {
     test('test the pingpong protocol', () {
+      ForceServer fs = new ForceServer();
+      
+      PlayPingPong playPingPong = new PlayPingPong();
+      fs.addProtocol(playPingPong.protocol());
+          
+      playPingPong.state_of_ping_pong.listen((state) {
+         // print ("coming in $state");
+      });
       
     });
   });
